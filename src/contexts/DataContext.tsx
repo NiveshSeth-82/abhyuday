aa56bb2a30144f85bfedc083546e3a7929b0045e
Nivesh <niveshseth432@gmail.com>
2026-02-23 12:45:37 +0530

    feat: add Aditya Dubey profile photo

--- src/contexts/DataContext.tsx
+++ src/contexts/DataContext.tsx
@@ -125,7 +125,7 @@ const defaultCoordinators: Coordinator[] = [
   { id: "c1", name: "Pankaj Srivastava", photo: "/coordinators/pankaj-srivastava.png", role: "Faculty", phone: "+91 94551 06460", email: "", department: "Computer Science", social: {} },
   { id: "c2", name: "Er. Arvind Kumar", photo: "", role: "Faculty", phone: "+91 831 706 6128", email: "", department: "Computer Science", social: {} },
   { id: "c3", name: "Dr. Sharmila Singh", photo: "/coordinators/sharmila-singh.jpg", role: "Faculty", phone: "+91 831 706 6128", email: "", department: "MBA", social: {} },
-  { id: "c4", name: "Aditya Dubey", photo: "", role: "Student Coordinator", phone: "+91 89320 56388", email: "", department: "Computer Science", social: {} },
+  { id: "c4", name: "Aditya Dubey", photo: "/coordinators/aditya-dubey.png", role: "Student Coordinator", phone: "+91 89320 56388", email: "", department: "Computer Science", social: {} },
   { id: "c5", name: "Diwakar Patel", photo: "", role: "Student Coordinator", phone: "+91 92355 08350", email: "", department: "Computer Science", social: {} },
   { id: "c6", name: "Sobrat Dayal", photo: "/coordinators/sobrat-dayal.png", role: "Student Coordinator", phone: "+91 95656 76988", email: "", department: "Computer Science", social: {} },
 ];
